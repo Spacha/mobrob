f165fef323867b1b4ba914c1387a78d44dec5d83
Spacha <miikasikala96@gmail.com>
2023-03-14 18:19:37 +0200

    Added UDP receiver which can now receive messages from ESP32

---
+++ SW/server/udp-receiver.py
@@ -0,0 +1,27 @@
+import socket
+
+UDP_IP = "192.168.1.154"
+UDP_PORT = 3333
+
+BUFSIZE = 1024
+TIMEOUT = 1
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+# looks like timeout is needed in order to receive input from the keyboard (e.g. KeyboardInterrupt)
+sock.settimeout(TIMEOUT)
+
+print(f"Listening at {UDP_PORT}...")
+
+while True:
+    try:
+        data, addr = sock.recvfrom(BUFSIZE)
+        print(f"received message [from client {addr}]: {data}")
+    except IOError as msg:
+        pass  # timed out, try again...
+    except KeyboardInterrupt:
+        break
+
+# cleanup if needed...
+print("Exiting...")
