2c0b3a9be4b3737a63fc1bb4a50b9f1d77d00503
Spacha <miikasikala96@gmail.com>
2024-06-21 04:16:44 +0300

    Added sample credentials file

---
+++ robot_sw/network_credentials.example.h
@@ -0,0 +1,10 @@
+#ifndef __NETWORK_CREDENTIALS_H__
+#define __NETWORK_CREDENTIALS_H__
+
+/////////////////////////////////////////////
+// Fill in your credentials here...
+#define NETWORK_NAME     ""
+#define NETWORK_PASSWORD ""
+/////////////////////////////////////////////
+
+#endif /* __NETWORK_CREDENTIALS_H__ */
